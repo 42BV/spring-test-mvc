abd82618b20bec7573680f56f300a06b690d4645
Rossen Stoyanchev <rstoyanchev@vmware.com>
2011-06-25 08:01:21 +0100

    Fix compile error in tests

--- src/test/java/org/springframework/test/web/server/setup/WebApplicationResourceAccessTests.java
+++ src/test/java/org/springframework/test/web/server/setup/WebApplicationResourceAccessTests.java
@@ -16,9 +16,22 @@
 
 package org.springframework.test.web.server.setup;
 
+import static org.springframework.test.web.server.MockHttpServletRequestBuilders.get;
+import static org.springframework.test.web.server.matcher.HandlerMatchers.handlerType;
+import static org.springframework.test.web.server.matcher.MvcResultMatchers.contentType;
+import static org.springframework.test.web.server.matcher.MvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.server.matcher.MvcResultMatchers.responseBodyContains;
+import static org.springframework.test.web.server.matcher.MvcResultMatchers.status;
+import static org.springframework.test.web.server.setup.MockMvcBuilders.annotationConfigMvcSetup;
+import static org.springframework.test.web.server.setup.MockMvcBuilders.xmlConfigMvcSetup;
+
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
@@ -26,7 +39,7 @@ import org.springframework.test.web.server.MockMvc;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.resource.DefaultServletHttpRequestHandler;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
@@ -34,49 +47,38 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles2.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles2.TilesView;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-
-import static org.springframework.test.web.server.MockHttpServletRequestBuilders.get;
-import static org.springframework.test.web.server.matcher.HandlerMatchers.handlerType;
-import static org.springframework.test.web.server.matcher.MvcResultMatchers.*;
-import static org.springframework.test.web.server.setup.MockMvcBuilders.annotationConfigMvcSetup;
-import static org.springframework.test.web.server.setup.MockMvcBuilders.xmlConfigMvcSetup;
-
 /**
  * Test access to web application resources through the MockServletContext.
- * The WAR root may be file system based or classpath-relative.
- *
+ * For example Tiles configuration, serving resources like .js, .css, etc.
+ * The WAR root may be file system or classpath-relative.
  */
 @RunWith(Parameterized.class)
-public class WebResourceTests {
+public class WebApplicationResourceAccessTests {
 
 	@Parameters
 	public static Collection<Object[]> parameters() {
-		return Arrays.asList(new Object[][] { 
-				{ ConfigType.XML, "src/test/webapp", false }, 
-				{ ConfigType.XML, "META-INF/web-resources", true },
-				{ ConfigType.ANNOT, "src/test/webapp", false }, 
-				{ ConfigType.ANNOT, "META-INF/web-resources", true }
+		return Arrays.asList(new Object[][] {
+				{ "src/test/webapp", true, false },
+				{ "META-INF/web-resources", true, true },
+				{ "src/test/webapp", false, false },
+				{ "META-INF/web-resources", false, true }
 		});
 	}
 	
 	private MockMvc mockMvc;
 	
-	public WebResourceTests(ConfigType configType, String webResourcePath, boolean isClasspathRelative) {
+	public WebApplicationResourceAccessTests(String webResourcePath, boolean isXmlConfig, boolean isClasspathRelative) {
 		
-		if (ConfigType.XML.equals(configType)) {
-			String location = "classpath:org/springframework/test/web/server/setup/servlet-context.xml";
-			mockMvc = xmlConfigMvcSetup(location)
-				.configureWarRootDir(webResourcePath, isClasspathRelative)
-				.build();
+		if (!isXmlConfig) {
+			mockMvc = annotationConfigMvcSetup(TestConfiguration.class)
+						.configureWarRootDir(webResourcePath, isClasspathRelative)
+						.build();
 		}
 		else {
-			mockMvc = annotationConfigMvcSetup(TestConfiguration.class)
-				.configureWarRootDir(webResourcePath, isClasspathRelative)
-				.build();
+			String location = "classpath:org/springframework/test/web/server/setup/servlet-context.xml";
+			mockMvc = xmlConfigMvcSetup(location)
+						.configureWarRootDir(webResourcePath, isClasspathRelative)
+						.build();
 		}
 	}
 	
@@ -112,8 +114,8 @@ public class WebResourceTests {
 	static class TestConfiguration extends WebMvcConfigurerAdapter {
 
 		@Override
-		public void configureResourceHandling(ResourceConfigurer configurer) {
-			configurer.addPathMapping("/resources/**").addResourceLocation("/resources/");
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 		}
 
 		@Override
@@ -140,7 +142,5 @@ public class WebResourceTests {
 			return new TestController();
 		}
 	}
-	
-	public enum ConfigType { XML, ANNOT }
 	
 }
