081875210a46be907e9fcec6f5b69792ebd95ccc
Keesun Baik <whiteship@epril.com>
2011-10-07 17:49:07 +0900

    added isNotFound(String msg)

--- src/main/java/org/springframework/test/web/server/result/StatusResultMatchers.java
+++ src/main/java/org/springframework/test/web/server/result/StatusResultMatchers.java
@@ -59,6 +59,21 @@ public class StatusResultMatchers {
     }
 
     /**
+     * Convenience Method for {@code HttpStatus.NOT_FOUND}
+     * Check if the HTTP is code is 404 and the error message.
+     * @param reason expected error message.
+     * @return true if the is code is 404 and the error message is equal.
+     */
+    public ResultMatcher isNotFound(final String reason){
+        return new AbstractServletResponseResultMatcher() {
+			protected void matchResponse(MockHttpServletResponse response) {
+				assertEquals("Status", HttpStatus.NOT_FOUND, HttpStatus.valueOf(response.getStatus()));
+                assertEquals("Error Message", reason, response.getErrorMessage());
+			}
+		};
+    }
+
+    /**
      * Convenience Method for {@code HttpStatus.CONTINUE}
      * Check if the HTTP is code is 100 or not.
      * @return true if the is code is 100.
